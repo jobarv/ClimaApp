43945d32cf322669ca7769b60d6d69e2a2d610ad
jobarv <jbarrera@vabaja.com.mx>
2023-01-12 14:21:52 -0600

    primer commit

---
+++ js/main.js
@@ -0,0 +1,17 @@
+const container = document.querySelector('.container');
+const resultado = document.querySelector('#resultado');
+const formulario = document.querySelector('#formulario');
+
+window.addEventListener(loa, () => {
+    formulario.addEventListener('submit', buscarClima);
+})
+
+function buscarClima(e) {
+    e.preventDefault();
+
+    //Validar
+    const ciudad = document.querySelector()
+
+
+    //Consultamos la API
+}
